9e2500ee853919fed46313ab4ab50eabaf2401e9
Sidhi Raj <siddhir954@gmail.com>
2026-02-02 22:05:16 +0530

    Implementing role based authorization

--- src/main/java/org/example/GreetingsController.java
+++ src/main/java/org/example/GreetingsController.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,17 @@ public class GreetingsController {
     @GetMapping("/greet")
     public String displayMessage(){
         return "Hello World!";
+    }
+
+    @PreAuthorize("hasRole('USER)")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "Hello User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "Hello Admin";
     }
 }
